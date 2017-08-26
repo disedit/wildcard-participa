9dbdaa391ea967692a6e92b014a3a44cce0cb476
Guillem <guillem@disedit.com>
2017-08-26 20:16:11 +0200

    Add Interface font as default option

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -11,7 +11,7 @@
 
     <title>{{ config('app.name', 'Participa') }}</title>
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
+    <link href="https://rsms.me/interface/interface.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
