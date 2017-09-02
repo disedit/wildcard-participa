b15dd07701b40dc49f57b09f1d8599b66422f627
Guillem <guillem@disedit.com>
2017-09-02 16:18:05 +0200

    Update login form to bootstrap 4.0beta

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,29 +9,29 @@
             <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                 {{ csrf_field() }}
 
-                <div class="form-group{{ $errors->has('username') ? ' has-danger' : '' }}">
+                <div class="form-group">
                     <label for="username" class="sr-only">Username</label>
                     <div class="input-group input-group-lg">
                         <span class="input-group-addon" id="user-icon"><i class="fa fa-user" aria-hidden="true"></i></span>
-                        <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" placeholder="Usuari" required autofocus aria-describedby="user-icon">
+                        <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" placeholder="Usuari" required autofocus aria-describedby="user-icon">
                     </div>
 
                     @if ($errors->has('username'))
-                        <span class="form-control-feedback">
+                        <span class="invalid-feedback d-block">
                             <strong>{{ $errors->first('username') }}</strong>
                         </span>
                     @endif
                 </div>
 
-                <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                <div class="form-group">
                     <label for="password" class="sr-only">Password</label>
                     <div class="input-group input-group-lg">
                         <span class="input-group-addon" id="key-icon"><i class="fa fa-lock" aria-hidden="true"></i></span>
-                        <input id="password" type="password" class="form-control" name="password" placeholder="Contrasenya" required>
+                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Contrasenya" required>
                     </div>
 
                     @if ($errors->has('password'))
-                        <span class="form-control-feedback">
+                        <span class="invalid-feedback d-block">
                             <strong>{{ $errors->first('password') }}</strong>
                         </span>
                     @endif
