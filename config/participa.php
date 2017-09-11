07ad757e9079f90c10a8b7a2108957fb5a791596
Guillem <guillem@disedit.com>
2017-09-11 19:27:34 +0200

    Add Google Analytics tracking option

--- config/participa.php
+++ config/participa.php
@@ -69,6 +69,9 @@ return [
     /* Whether to display total census number on public result pages */
     'display_census_number' => true,
 
+    /* Set up Google Analytics tracking */
+    'google_analytics_ID' => 'UA-106217417-1',
+
     /*
     |--------------------------------------------------------------------------
     | Languages
