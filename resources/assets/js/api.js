448549bd0eefef07e1d6e80e1a17377b4323f150
Guillem <guillem@disedit.com>
2017-09-14 18:51:07 +0200

    If an AJAX request returns a non-JSON formatted error, create a generic error object to pass to the Vue app

--- resources/assets/js/api.js
+++ resources/assets/js/api.js
@@ -38,12 +38,16 @@ export default class Participa {
 
     _call(type, url, data) {
         return new Promise((resolve, reject) => {
-            axios[type](this.apiURL + url, data)
-                .then(response => {
-                    resolve(response.data);
-            })
-            .catch(error => {
-                reject(error.response.data);
+            axios[type](this.apiURL + url, data).then(response => {
+                resolve(response.data);
+            }).catch(error => {
+                if(error.response.status == 500) {
+                    reject({
+                        'error': ['Error del sistema. És possible que aquest error siga temporal. Refresca la pàgina i torna a intentar-ho o posa\'t en contacte.']
+                    });
+                } else {
+                    reject(error.response.data);
+                }
             });
         });
     }
