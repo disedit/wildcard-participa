54deeba8251a50de4750564d333d9881530a0594
Guillem <guillem@disedit.com>
2018-05-01 12:02:28 +0200

    Display cta button only if $edition is set

--- resources/views/components/voteinfo.blade.php
+++ resources/views/components/voteinfo.blade.php
@@ -6,17 +6,20 @@
             <div class="col-md-8 vote-info__text">
                 <h2>@lang('participa.heading')</h2>
                 <p class="vote-info__intro">@lang('participa.subheading')</p>
-                <p class="vote-info__action">
-                    @if($edition->isOpen())
-                        <a href="#content" class="big-button"><i aria-hidden="true" class="fa fa-bullhorn"></i> Vote</a>
-                    @elseif($edition->inProposalPhase())
-                        <a href="{{ secure_url('propose') }}" class="big-button"><i aria-hidden="true" class="fa fa-comment"></i> Envia la teua proposa</a>
-                    @elseif($edition->isPending())
-                        <a href="#content" class="big-button"><i aria-hidden="true" class="fa fa-info-circle"></i> Més informació</a>
-                    @elseif($edition->resultsPublished())
-                        <a href="#content" class="big-button"><i aria-hidden="true" class="fa fa-chart-bar"></i> Resultats</a>
-                    @endif
-                </p>
+
+                @if(isset($edition))
+                    <p class="vote-info__action">
+                        @if($edition->isOpen())
+                            <a href="#content" class="big-button"><i aria-hidden="true" class="fa fa-bullhorn"></i> Vote</a>
+                        @elseif($edition->inProposalPhase())
+                            <a href="{{ secure_url('propose') }}" class="big-button"><i aria-hidden="true" class="fa fa-comment"></i> Envia la teua proposa</a>
+                        @elseif($edition->isPending())
+                            <a href="#content" class="big-button"><i aria-hidden="true" class="fa fa-info-circle"></i> Més informació</a>
+                        @elseif($edition->resultsPublished())
+                            <a href="#content" class="big-button"><i aria-hidden="true" class="fa fa-chart-bar"></i> Resultats</a>
+                        @endif
+                    </p>
+                @endif
             </div>
 
             <div class="col-md-4">
