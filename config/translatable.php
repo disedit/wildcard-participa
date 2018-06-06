e5e86c1b3f060d7e9ba90ef2ab81f5ad8abae9d9
Guillem <guillem@disedit.com>
2018-06-06 19:15:19 +0200

    Enable translation fallbacks

--- config/translatable.php
+++ config/translatable.php
@@ -51,7 +51,7 @@ return [
     | $useTranslationFallback when defined
     |
     */
-    'use_fallback' => false,
+    'use_fallback' => true,
 
     /*
     |--------------------------------------------------------------------------
