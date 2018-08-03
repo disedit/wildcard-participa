3e5b1bf48d827cbe23956465667b42a290ae5af6
Guillem <guillem@disedit.com>
2018-08-03 21:24:07 +0200

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,8 +16,9 @@ mix.js('resources/assets/js/app.js', 'public/js')
    .js('resources/assets/js/common.js', 'public/js')
    .js('resources/assets/js/admin.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .sass('resources/assets/sass/admin.scss', 'public/css');
+   .sass('resources/assets/sass/admin.scss', 'public/css')
+   .copyDirectory('resources/assets/images', 'public/images');
 
-if (mix.config.inProduction) {
+if (mix.inProduction) {
     mix.version();
 }
