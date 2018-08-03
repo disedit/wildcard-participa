5ba47b97f0e32ad74d7b2271c0281136c341bc71
Guillem <guillem@disedit.com>
2018-08-03 23:38:30 +0200

    Do not include fontawesome css if package not installed

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -16,7 +16,9 @@
 
     <link href="https://fonts.googleapis.com/css?family=Muli:400,600,700,900" rel="stylesheet">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <link href="{{ mix('css/fontawesome.css') }}" rel="stylesheet">
+    @if(file_exists(public_path('css/fontawesome.css')))
+        <link href="{{ mix('css/fontawesome.css') }}" rel="stylesheet">
+    @endif
 
     @include('components.metatags')
 </head>
